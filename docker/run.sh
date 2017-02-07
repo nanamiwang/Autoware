d8067f504c8725bf82de2d2a57e430ca10c666fe
Alex <wangzhe.nanami@gmail.com>
2017-02-07 12:53:03 +0800

    Fix window geometry issue

--- docker/run.sh
+++ docker/run.sh
@@ -2,12 +2,16 @@
 
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
-
+touch $XAUTH
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f ${XAUTH} nmerge -
 nvidia-docker run \
         -it --rm \
         --volume=$XSOCK:$XSOCK:rw \
         --volume=$XAUTH:$XAUTH:rw \
+	--volume=/home/nanami/.autoware:/home/autoware/.autoware \
         --env="XAUTHORITY=${XAUTH}" \
         --env="DISPLAY=${DISPLAY}" \
-        -u autoware \
+	--env="QT_X11_NO_MITSHM=1" \
+	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+	-u autoware \
         autoware-image
